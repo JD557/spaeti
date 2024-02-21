763013a6247cb423f491533863b1205683e2c7de
Scala Steward <scala_steward@virtuslab.com>
2024-02-21 21:10:13 +0000

    Update munit to 1.0.0-M11

--- project.scala
+++ project.scala
@@ -5,7 +5,7 @@
 //> using dep io.get-coursier:coursier_2.13:2.1.8
 //> using dep io.get-coursier:coursier-install_2.13:2.1.8
 
-//> using test.dep org.scalameta::munit::1.0.0-M10
+//> using test.dep org.scalameta::munit::1.0.0-M11
 
 //> using resource-dir src/main/resources/
 //> using option -deprecation
