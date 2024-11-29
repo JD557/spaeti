9e8a9de93bab199a01e293e538be6bacb992a425
Scala Steward <scala_steward@virtuslab.com>
2024-11-29 16:34:00 +0000

    Update coursier-install_2.13, ... to 2.1.19

--- project.scala
+++ project.scala
@@ -2,8 +2,8 @@
 
 //> using dep eu.joaocosta::minart::0.6.2
 //> using dep eu.joaocosta::interim::0.2.0
-//> using dep io.get-coursier:coursier_2.13:2.1.17
-//> using dep io.get-coursier:coursier-install_2.13:2.1.17
+//> using dep io.get-coursier:coursier_2.13:2.1.19
+//> using dep io.get-coursier:coursier-install_2.13:2.1.19
 
 //> using test.dep org.scalameta::munit::1.0.2
 
